6de5c97687b3afea2ad3dbb0052e93d6c8c0213c
edivaldo junior <juniorug@gmail.com>
2020-10-07 23:36:50 -0300

    adding formsModule and HttpClientModule to appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateCraftBeerComponent } from './create-craft-beer/create-craft-beer.component';
@@ -18,7 +19,9 @@ import { UpdateCraftBeerComponent } from './update-craft-beer/update-craft-beer.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
